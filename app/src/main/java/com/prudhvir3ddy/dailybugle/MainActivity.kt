9533fb655bdf90943cfaa627c6e8436aa2a3e237
prudhvi reddy <prudhvireddy.m01@gmail.com>
2019-12-15 14:36:27 +0530

    added api key config

--- app/src/main/java/com/prudhvir3ddy/dailybugle/MainActivity.kt
+++ app/src/main/java/com/prudhvir3ddy/dailybugle/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.prudhvir3ddy.dailybugle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, BuildConfig.apiNews, Toast.LENGTH_SHORT).show()
     }
 }
